a7572e249b3c72256fc5bada86a6bc0ef159896c
Eric Shull <eshulldesign@gmail.com>
2018-09-21 12:31:24 -0700

    add more code to try and hide every product on home page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
+    @product = @products.new(params[:id])
   end
 
   def new
