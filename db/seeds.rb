19602ea95a808bd7691034cc4e46441f4981a114
Student <example@example.com>
2018-09-27 13:21:05 -0700

    add admin priveleges for new product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@ class Seed
     seed.generate_products
   end
 
+  def generate_users
+    User.destroy_all
+    User.create!(name: "admin", email: "admin@admin.com", password: "New1234!!!!", admin: true)
+    User.create!(name: "user", email: "user@user.com", password: "New1234!!!!")
+  end
+
   def generate_products
     20.times do |i|
       Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
